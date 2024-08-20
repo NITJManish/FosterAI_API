ae2c7808af4c410a0b769679a428e6b2033a1a32
neera <kumarmanish150799@gmail.com>
2024-08-20 15:43:33 +0530

    database connection and custom error handling

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { connectDatabase } from "./config/connectDB.js";
+import errorMiddleware from "./middleware/errors.js";
+
+
+// Handle Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down due to uncaught expection");
+  process.exit(1);
+});
+
+dotenv.config({ path: "./config/config.env" });
+
+// Connecting to database
+connectDatabase();
+
+app.use(
+  express.json()
+);
+app.use(cookieParser());
+
+app.get("/",(req,res)=>{
+    res.send("hell i am back");
+})
+
+
+// Using error middleware
+app.use(errorMiddleware);
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
+
+//Handle Unhandled Promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down server due to Unhandled Promise Rejection");
+  server.close(() => {
+    process.exit(1);
+  });
+});
